98ddbd198bee01aa22acc3f4a6ae5f5c9600deb1
ibrahimelnemr <70034333+ibrahimelnemr@users.noreply.github.com>
2025-03-04 16:25:55 +0200

    set up index, home, baseLayout, navbar, footer

--- frontend/src/layouts/BaseLayout.tsx
+++ frontend/src/layouts/BaseLayout.tsx
@@ -1,9 +1,17 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import { ReactNode } from "react";
 
 export default function BaseLayout({ children }: { children: ReactNode }) {
 
-    <div>
-        
-    </div>
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Navbar />
+            <main className="relative flex-grow">
+                {children}
+            </main>
+            <Footer />
+        </div>
+    );
 
 }
