f4b5e385143791468090f1f22a3d5e48bb120989
ibrahimelnemr <70034333+ibrahimelnemr@users.noreply.github.com>
2025-03-04 17:55:17 +0200

    add Data.ts

---
+++ frontend/src/data/Data.ts
@@ -0,0 +1,12 @@
+const Employees = [
+    {
+        id: 1,
+        name: "John Doe",
+        title: "Software Engineer",
+        skills: ["React", "Node.js", "TypeScript"]
+    }
+]
+
+export { 
+    Employees
+};
