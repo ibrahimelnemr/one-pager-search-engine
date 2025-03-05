1ec212ff78a0a6f26163609b254611bab74fe493
ibrahimelnemr <70034333+ibrahimelnemr@users.noreply.github.com>
2025-03-05 13:43:25 +0200

    separate one pager data files

---
+++ frontend/src/data/RealOnePagerData.ts
@@ -0,0 +1,33 @@
+export const RealOnePagerData = [
+    {
+        username: "ielnemr",
+        email: "ielnemr@deloitte.com",
+        dpnProfileLink: "https://people.deloitte/profile/ielnemr",
+        profileImage: "https://raw.githubusercontent.com/ibrahimelnemr/one-pager-search-engine/refs/heads/main/frontend/public/images/Ibrahim%20El%20Nemr.jpg",
+        name: "Ibrahim El Nemr",
+        title: "Software Engineer",
+        officialTitle: "Analyst",
+        costCenter: "T&T, EAID, Customer As a Service / Operate",
+        businessSkills: ["Adaptability", "Communication"],
+        technologySkills: ["Programming languages: Java, Python, TypeScript, SQL Databases: PostgreSQL, MongoDB Frameworks: React.js, Spring Boot, Node.js, MongoDB, Other: Git, Agile"],
+        industryExperience: ["Ecommernce, Banking, Education"],
+        education: "BA in Economics, minor in Computer Science, the American University in Cairo",
+        languages: "Arabic English French",
+        certificates: "Udacity Full Stack JavaScript Developer Nanodegree Udacity iOS Developer Nanodegree",
+        summaryOfProfessionalExperience: "Ibrahim El Nemr is a full-stack developer with extensive experience in end-to-end development across a variety of frameworks and technologies, including Spring Boot, PostgreSQL, React.js, Node.js, MongoDB, iOS and Flutter. He demonstrates exceptional flexibility and a strong willingness to learn new frameworks and technology stacks, adapting to business and technology requirements to deliver quality results in collaboration with cross-functional teams.",
+        relevantExperience: `Full Stack Engineer, Flagak
+        Coordinated with CTO to design and implement new backend in Java Spring Boot and PostgreSQL using layered architecture
+        Implemented admin APIs on Node TypeScript backend with MongoDB and mongoose
+        Added features to ecommerce admin management site using React, TypeScript and Material UI
+        
+        Back-End Developer, AUC Student Union
+        Implemented new backend features for a mobile application being used by 7000+ students
+        Added features to ecommerce admin management site using React, TypeScript and Material UI
+        
+        Technical Intern, CIB
+        Explored uses of SAS for data analytics
+        Researched and proposed solutions for financial data analysis and reporting `,
+        selectedClients: ""
+
+    }
+]
